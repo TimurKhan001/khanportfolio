b02ea8b075ed9d568363b22c96daa987542c1887
EC2 Default User <ec2-user@ip-172-31-10-51.eu-central-1.compute.internal>
2021-04-21 16:05:17 +0000

    added a handleform

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import Media from 'react-media'; 
 import "./Contact.css";
 import {ReactComponent as GitLogo} from './img/github-outline_1.svg';
@@ -10,6 +10,31 @@ import {ReactComponent as EmailLogoWhite} from './img/email-outline.svg';
 import useWindowDimensions from "./WindowDimension.js";
 
 const Contact = () => {
+
+    //Form manipulation  
+const [status, setStatus] = useState("Submit");    
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+
+let response = await window.fetch("http://35.158.111.241:8081/contact", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+ setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+    window.location.reload();
+  };    
     
 const { height } = useWindowDimensions();
     
@@ -28,21 +53,21 @@ return (
                         <div style={{height: height - 162}} className="contact__mobile-content">
                             <h3>I'm available for hire!</h3>
                             
-                            <form className="contact-form" method="POST">
+                            <form className="contact-form" onSubmit={handleSubmit}>
                                 <div className="form-group">
-                                    <input type="text" className="form-control" placeholder="Name" />
+                                    <input type="text" className="form-control" placeholder="Name" id="name" required  />
                                 </div>
                                     
                                 <div className="form-group">
-                                    <input type="email" className="form-control" placeholder="Email" />
+                                    <input type="email" className="form-control" placeholder="Email" id="email" required />
                                 </div>
                                     
                                 <div className="form-group">
-                                    <textarea className="form-control" rows="5" placeholder="Type your message"></textarea>
+                                    <textarea className="form-control" rows="5" placeholder="Type your message" id="message" required></textarea>
                                 </div>
                                     
                                 <div className="form-group">
-                                <button type="submit" className="btn">Submit</button>
+                                <button style={{cursor: "pointer"}} type="submit" className="btn">Send</button>
                                 </div>
                                     
                             </form>
@@ -68,21 +93,21 @@ return (
                             <div className="contact__laptop-text mt-big">
                                 <p>You can contact me by using the contact form below or by sending an e-mail to timurkhan@seznam.cz. I always respond to messages within 24 hrs.</p>
                                 
-                                <form className="contact-form" method="POST">
+                                <form className="contact-form" onSubmit={handleSubmit}>
                                     <div className="form-group mt-big">
-                                        <input type="text" className="form-control" placeholder="Name" />
+                                        <input type="text" className="form-control" placeholder="Name" id="name" required/>
                                     </div>
                                         
                                     <div className="form-group mt-medium">
-                                        <input type="email" className="form-control" placeholder="Email" />
+                                        <input type="email" className="form-control" placeholder="Email" id="email" required/>
                                     </div>
                                         
                                     <div className="form-group mt-medium">
-                                        <textarea className="form-control" rows="5" placeholder="Type your message"></textarea>
+                                        <textarea className="form-control" rows="5" placeholder="Type your message" id="message" required></textarea>
                                     </div>
                                         
                                     <div className="form-group mt-big">
-                                        <button type="submit" className="btn">Send</button>
+                                        <button style={{cursor: "pointer"}} type="submit" className="btn">Send</button>
                                     </div>
                                 </form>
                             </div>
